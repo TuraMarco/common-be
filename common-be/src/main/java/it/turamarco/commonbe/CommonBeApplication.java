bb4a99425755a24d685995ffba5f8acd6e2deb20
turam <turam@PC_MARCO>
2023-10-06 22:04:55 +0200

    first commit

---
+++ common-be/src/main/java/it/turamarco/commonbe/CommonBeApplication.java
@@ -0,0 +1,13 @@
+package it.turamarco.commonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonBeApplication.class, args);
+	}
+
+}
---
+++ common-be/src/test/java/it/turamarco/commonbe/CommonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.turamarco.commonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
